c9eda60d20de145900196469eae8c7f0b80ea048
CoopusDev <copperingot@163.com>
2022-12-28 13:21:25 +0800

    更改了初始化方式

--- src/app.js
+++ src/app.js
@@ -2,12 +2,29 @@ import Point from "./objects/point.js";
 import Segment from "./objects/segment.js";
 import Line from "./objects/line.js"
 import Circle from "./objects/circle.js"
-import Polygon from "./objects/polygon.js"
 import animation from "./animation/animation.js"
 
 const mraph = {
     elements: [],
-    
+
+    //初始化
+    setup(container, config) {
+        const main = document.createElement("canvas");
+        container.appendChild(main);
+
+        if (config) {
+            if (config.width) {
+                main.width = 3 * config.width;
+                main.style.width = config.width + "px";
+            }
+            if (config.height) {
+                main.height = 3 * config.height;
+                main.style.height = config.height + "px";
+            }
+        }
+        
+        this.canvas = main;
+    },
     // 绘制全体
     draw() {
         const els = this.elements;
@@ -18,7 +35,7 @@ const mraph = {
     // 设置背景
     background(color = "white") {
         this.ctx2d.fillStyle = color;
-        
+
         const w = this.canvas.width;
         const h = this.canvas.height;
         this.ctx2d.fillRect(-w/2, -h/2, w, h);
@@ -34,15 +51,16 @@ const mraph = {
     //从点获取数组
     getPos(point) {
         if (point instanceof Point) {
-            return [point.x, point.y];
+            return [point.x,
+                point.y];
         } else {
             return point;
         }
     },
-    
+
     set canvas(canvas) {
         this._canvas = canvas;
-        
+
         const ctx = canvas.getContext("2d");
         this.ctx2d = ctx;
         ctx.translate(canvas.width / 2, canvas.height / 2);
@@ -56,7 +74,6 @@ mraph.Point = Point;
 mraph.Segment = Segment;
 mraph.Line = Line;
 mraph.Circle = Circle;
-mraph.Polygon = Polygon;
 mraph.animation = animation;
 
 window.mraph = mraph;
