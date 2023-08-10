64ba13f9b34475a6369c6d053914e71c57aa5bb4
CoopusDev <copperingot@163.com>
2023-08-10 11:15:43 +0800

    feat: changed the renderer usage

--- src/objects/Box.js
+++ src/objects/Box.js
@@ -1,6 +1,5 @@
 import Graph from "./Graph.js";
-import Vector from "../math/Vector.js";
-import Matrix from "../math/Matrix.js";
+// import Vector from "../math/Vector.js";
 
 export default class Box extends Graph {
     constructor(base, width, height, depth) {
@@ -11,11 +10,8 @@ export default class Box extends Graph {
         this.depth = depth;
     }
 
-    get path() {
-        const c = this.center.columns;
-        const mat = Matrix.translate(...c).mult(
-            this.matrix.mult(Matrix.translate(-c[0], -c[1], -c[2]))
-        );
+    /*get path() {
+        const mat = this.matrix;
         const BASE = this.base.pos.trans(mat);
         const UP = new Vector([0, this.height, 0, 0]).trans(mat);
         const RIGHT = new Vector([this.width, 0, 0, 0]).trans(mat);
@@ -30,6 +26,7 @@ export default class Box extends Graph {
             ["line", BASE.add(UP).add(RIGHT).reduce(UP)],
             ["close"],
             ["stroke"],
+            ["fill"],
 
             ["begin"],
             ["move", BASE.add(IN)],
@@ -38,27 +35,72 @@ export default class Box extends Graph {
             ["line", BASE.add(IN).add(UP).add(RIGHT).reduce(UP)],
             ["close"],
             ["stroke"],
+            ["fill"],
 
             ["begin"],
-            ["move", BASE],
-            ["line", BASE.add(IN)],
-            ["stroke"],
-
-            ["begin"],
-            ["move", BASE.add(UP)],
+            ["move", BASE.add(IN)],
             ["line", BASE.add(IN).add(UP)],
+            ["line", BASE.add(IN).add(UP).reduce(IN)],
+            ["line", BASE.add(IN).add(UP).reduce(IN).reduce(UP)],
+            ["close"],
             ["stroke"],
+            ["fill"],
 
             ["begin"],
-            ["move", BASE.add(UP).add(RIGHT)],
-            ["line", BASE.add(IN).add(UP).add(RIGHT)],
+            ["move", BASE.add(RIGHT).add(IN)],
+            ["line", BASE.add(RIGHT).add(IN).add(UP)],
+            ["line", BASE.add(RIGHT).add(IN).add(UP).reduce(IN)],
+            ["line", BASE.add(RIGHT).add(IN).add(UP).reduce(IN).reduce(UP)],
+            ["close"],
             ["stroke"],
+            ["fill"],
 
             ["begin"],
-            ["move", BASE.add(UP).add(RIGHT).reduce(UP)],
-            ["line", BASE.add(IN).add(UP).add(RIGHT).reduce(UP)],
+            ["move", BASE.add(IN)],
+            ["line", BASE.add(IN).add(RIGHT)],
+            ["line", BASE.add(IN).add(RIGHT).reduce(IN)],
+            ["line", BASE.add(IN).add(RIGHT).reduce(IN).reduce(RIGHT)],
+            ["close"],
             ["stroke"],
+            ["fill"],
+
+            ["begin"],
+            ["move", BASE.add(UP).add(IN)],
+            ["line", BASE.add(UP).add(IN).add(RIGHT)],
+            ["line", BASE.add(UP).add(IN).add(RIGHT).reduce(IN)],
+            ["line", BASE.add(UP).add(IN).add(RIGHT).reduce(IN).reduce(RIGHT)],
+            ["close"],
+            ["stroke"],
+            ["fill"],
         ];
+    }
+
+    render() {
+        if (!this.renderer || !this.visible) return this;
+
+        const renderer = this.renderer;
+        const mat = this.matrix;
+        const UP = new Vector([0, this.height, 0, 0]).trans(mat);
+        const RIGHT = new Vector([this.width, 0, 0, 0]).trans(mat);
+        const IN = new Vector([0, 0, this.depth, 0]).trans(mat);
+
+        const DLO = this.base.pos.trans(mat);
+        const DLI = BASE.add(IN);
+        const DRO = BASE.add(RIGHT);
+        const DRI = DRO.add(IN);
+        const ULO = BASE.add(UP);
+        const ULI = ULO.add(IN);
+        const URO = ULO.add(RIGHT);
+        const URI = URO.add(IN);
+
+        renderer.style(this);
+        renderer.begin();
+        renderer.move(DLO);
+        renderer.line(DRO);
+        renderer.line(URO);
+        renderer.line(ULO);
+        renderer.close();
+        renderer.stroke();
     }
 
     get center() {
@@ -66,5 +108,5 @@ export default class Box extends Graph {
         return base.add(
             new Vector([this.width / 2, this.height / 2, this.depth / 2, 0])
         );
-    }
+    }*/
 }
--- src/objects/Graph.js
+++ src/objects/Graph.js
@@ -4,6 +4,7 @@ export default class Graph {
     size = 7;
     dash = [];
     alpha = 1;
+    visible = true;
     fillColor = "white";
     strokeColor = "black";
     strokeWidth = 5;
